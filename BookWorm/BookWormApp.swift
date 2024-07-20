cf5f104cac6d8c041f26059ac268cdd76a96accc
Berlin <berlinpt@gmail.com>
2024-07-20 15:09:14 -0400

    Creating books with SwiftData

--- BookWorm/BookWormApp.swift
+++ BookWorm/BookWormApp.swift
@@ -5,6 +5,7 @@
 //  Created by Berlin Thomas on 2024-07-20.
 //
 
+import SwiftData
 import SwiftUI
 
 @main
@@ -13,5 +14,6 @@ struct BookWormApp: App {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(for: Book.self)
     }
 }
