726add81bbadf20bae3c495c909c90e65d2ffaa1
Berlin <berlinpt@gmail.com>
2024-07-20 14:21:38 -0400

    Initial Commit

---
+++ BookWorm/BookWormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookWormApp.swift
+//  BookWorm
+//
+//  Created by Berlin Thomas on 2024-07-20.
+//
+
+import SwiftUI
+
+@main
+struct BookWormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
